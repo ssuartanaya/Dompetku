45392d165189235fedb9fb8a01bf2a65279684ea
ssuartanaya <sugisuartanaya@gmail.com>
2018-02-22 11:10:21 -0800

    fixed Category table

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    @category = Category.new
   end
 
   # GET /categories/1
@@ -15,6 +16,7 @@ class CategoriesController < ApplicationController
   # GET /categories/new
   def new
     @category = Category.new
+    respond_with(@category)
   end
 
   # GET /categories/1/edit
@@ -25,40 +27,55 @@ class CategoriesController < ApplicationController
   # POST /categories.json
   def create
     @category = Category.new(category_params)
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
+    if @category.save
+      sweetalert_success('Your transaction successfully created.', 'Successfully created', persistent: 'Okay!') 
+      redirect_to categories_path
+    else
+      sweetalert_error('Sorry transaction unsuccessful.', 'Sorry...', persistent: 'Dismiss!') 
+      redirect_to categories_path
+    end  
+    # respond_to do |format|
+    #   if @category.save
+    #     format.html { redirect_to @category, notice: 'Category was successfully created.' }
+    #     format.json { render :show, status: :created, location: @category }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @category.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /categories/1
   # PATCH/PUT /categories/1.json
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.update(category_params)
+      sweetalert_success('Your transaction successfully updated.', 'Successfully updated', persistent: 'Okay!') 
+      redirect_to categories_path
+    else
+      sweetalert_error('Sorry transaction unsuccessful.', 'Sorry...', persistent: 'Dismiss!') 
+      redirect_to categories_path
     end
+    # respond_to do |format|
+    #   if @category.update(category_params)
+    #     format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @category }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @category.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /categories/1
   # DELETE /categories/1.json
   def destroy
     @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    sweetalert_success('Your transaction successfully deleted.', 'Successfully deleted', persistent: 'Okay!') 
+    redirect_to categories_path
+    # respond_to do |format|
+    #   format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
